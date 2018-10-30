318d41e18abcec82b42e0902fb1a58843c637ded
Joseph Isbell <shadowwolf347@comcast.net>
2018-10-29 18:17:49 -0600

    Added form group for password and radio buttons to store data.

--- src/App.vue
+++ src/App.vue
@@ -25,10 +25,33 @@
                     </div>
                     <div class="form-group">
                         <label for="email">Email</label>
-                        <input type="text"
+                        <input type="email"
                                 id="email"
                                 class="form-control"
                                 v-model="userData.email">
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input type="password"
+                                id="password"
+                                class="form-control"
+                                v-model="userData.password">
+                    </div>
+                    <div class="row">
+                        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
+                            <label for="stored">
+                                <input type="radio"
+                                        id="stored"
+                                        value="Yes"
+                                        v-model="storeData"> Yes
+                            </label>
+                            <label for="no">
+                                <input type="radio"
+                                        id="no"
+                                        value="No"
+                                        v-model="storeData"> No
+                            </label>
+                        </div>
                     </div>
 
                     <!-- Exercise 2 -->
@@ -51,8 +74,8 @@
                     <div class="panel-body">
                         <p>Full Name: {{ userData.firstName }} {{ userData.lastName }}</p>
                         <p>Mail: {{ userData.email }}</p>
-                        <p>Password: </p>
-                        <p>Store in Database?: </p>
+                        <p>Password: {{ userData.password }}</p>
+                        <p>Store in Database?: {{ storeData }}</p>
                     </div>
                 </div>
             </div>
@@ -70,7 +93,7 @@
                     eamail: '',
                     password: '',
                 },
-                storeData: true
+                storeData: 'Yes'
             }
         }
     }
