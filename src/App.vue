b38ec7ec80daaa84a8d8e338e5709f9ee6a3980f
Joseph Isbell <shadowwolf347@comcast.net>
2018-10-29 18:23:43 -0600

    Added a submit btn and submitted method.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container">
         <form>
-            <div class="row">
+            <div class="row" v-if="!isSubmitted">
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                     <!-- Exercise 1 -->
                     <!-- Create a Signup Form where you retrieve the following Information -->
@@ -39,6 +39,7 @@
                     </div>
                     <div class="row">
                         <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
+                            <label> Store Data?</label>
                             <label for="stored">
                                 <input type="radio"
                                         id="stored"
@@ -57,7 +58,7 @@
                     <!-- Exercise 2 -->
                     <!-- Only display the Form if it has NOT been submitted -->
                     <!-- Display the Data Summary ONCE the Form HAS been submitted -->
-
+                    <button class="btn btn-primary" @click.prevent="submitted">Submit!</button>
                     <!-- Exercise 3 -->
                     <!-- Edit the Example from above and create a custom "Full Name" Control -->
                     <!-- which still holds the First Name and Last Name Input Field -->
@@ -65,7 +66,7 @@
             </div>
         </form>
         <hr>
-        <div class="row">
+        <div class="row" v-if="isSubmitted">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <div class="panel panel-default">
                     <div class="panel-heading">
@@ -93,7 +94,13 @@
                     eamail: '',
                     password: '',
                 },
-                storeData: 'Yes'
+                storeData: 'Yes',
+                isSubmitted: false
+            }
+        },
+        methods: {
+            submitted(){
+                this.isSubmitted = true;
             }
         }
     }
